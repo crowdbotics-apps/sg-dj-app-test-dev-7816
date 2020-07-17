a8cef1ec0b38a1468ed49a243b163543575db20a
Crowdbotics <team@crowdbotics.com>
2020-07-17 14:46:37 +0000

    Update models

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sg_dj_app_test_dev_7816.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sg_dj_app_test_dev_7816.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- sg_dj_app_test_dev_7816/wsgi.py
+++ sg_dj_app_test_dev_7816/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sg_dj_app_test_dev_7816.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sg_dj_app_test_dev_7816.settings")
 
 application = get_wsgi_application()
